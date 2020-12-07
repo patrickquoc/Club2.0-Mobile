c8fbef5c9c6295a5ae2af5d2aed503405b4c9ad9
Patrick Quoc <quoc.patrick@hotmail.com>
2020-12-07 13:55:33 +0100

    Refactoring

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  connection: 'http://vm130.htl-leonding.ac.at:8443',
+  wsConnection: 'https://vm130.htl-leonding.ac.at:8080'
 };
